964ef0db88fcdcfc60bb8ed3a0fde80c87495350
Dylan Zenner <54154602+zennerdylan@users.noreply.github.com>
2019-10-28 14:49:37 -0500

    Update and rename Exercises to Exercises.py

---
+++ Chapter_2_Variables_&_Data_Types/Exercises.py
@@ -0,0 +1,17 @@
+# 2.1 Simple Message: Store a message in a variable, and then print that message.
+
+variable1 = 'Hello World!'
+
+print(variable1)
+
+
+# 2.2 Simple Messages: Store a message in a variable, and print that message. Then change the value of your variable to
+# a new message, and print the new message.
+
+variable2 = 'Hello python community!'
+
+print(variable2)
+
+variable2 = 'Let's explore the world of coding together!'
+
+print(variable2)
