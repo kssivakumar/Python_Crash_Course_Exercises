9f606411e041a5cd46f48f34324875e0348b1771
Dylan Zenner <54154602+zennerdylan@users.noreply.github.com>
2019-10-30 15:15:36 -0500

    Update Exercises.py

--- Chapter_5_If_Statements/Exercises.py
+++ Chapter_5_If_Statements/Exercises.py
@@ -174,75 +174,117 @@ else:
     print('You just earned 15 points!')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 5.6 Stages Of Life: Write an if-elif-else chain that determines a person's stage of life. Set a value for the lariable age, and then:
+# - If the person is less than 2 years old, print a message that the person is a baby.
+# - If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+# - If the person is at least 4 years old but less than 13 print a message that the person is a kid.
+# - If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+# - If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+# - If the person is age 65 or older, print a message that the person is an elder.
+
+# I used the random module here so that when we do not have to hard code the age of the person we are looking. Instead
+# we can just run the program and get a random age between 0 and 100 and the program will automatically tell us what stage of life they are in.
+
+import random
+age = range(101)
+if random.choice(age) < 2:
+    print('This person is a baby.')
+elif random.choice(age) >= 2 and random.choice(age) < 4:
+    print('This person is a toddler.')
+elif random.choice(age) >= 4 and random.choice(age) < 13:
+    print('This person is a kid.')
+elif random.choice(age) >= 13 and random.choice(age) < 20:
+    print('This person is a teenager.')
+elif random.choice(age) >= 20 and random.choice(age) < 65:
+    print('This person is an adult.')
+else:
+    print('This person is an elder.')
+
+
+# 5.7 Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent if statements that check for certain 
+# fruits in your list.
+# - Make a list of your favorite fruits and call it favorite_fruits.
+# - Write five if statements. Each should check whether a certain kind of fruit is in your list.
+# if the fruit is in your list, the if block should print a statement, such as 'You really like bananas!'
+
+# I thought the way this question wanted us to do the problem was ineffective and had a slower run time so I did a for loop.
+favorite_fruits = ['Strawberries', 'Blueberries', 'Peaches', 'Apples', 'Raspberries']
+
+for fruit in favorite_fruits:
+    if fruit in favorite_fruits:
+        print('You really like ' + fruit + '!')
+        
+        
+# 5.8 Hello Admin: Make a list of five or more usernames, including tha name 'admin'. Imagine that you are writing code
+# that will print a greeting to each user after they log into a website. Loop through the list, and print a greeting to each user.
+# - If the username is 'admin', print a special greeting, such as 'Hello admin, would you like to see a status report?'
+# - Otherwise, print a generic greeting, such as 'Hello Eric, thank you for logging in again.'
+
+usernames = ['admin', 'jake', 'brice', 'dylan', 'eli']
+
+for name in usernames:
+    if name == 'admin':
+        print('Hello admin, would you like to see a status report?')
+    else:
+        print('Hello ' + name + ', thank you for logging in again.')
+        
+# 5.9 No Users: Add an if test to the previous exercise to make sure that the list is not empty.
+# - If the list is empty, print the message 'We need to find some users!'
+# - Remove all of the usernames from your list, and make sure the correct message is printed.
+
+usernames = ['admin', 'jake', 'brice', 'dylan', 'eli']
+
+if usernames == []:
+    print('We need to find some users!')
+elif len(usernames) > 0:
+    for name in usernames:
+        if name == 'admin':
+            print('Hello admin, would you like to see a status report?')
+        else:
+            print('Hello ' + name + ', thank you for logging in again!')
+            
+# 5.10 Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone
+# has a unique username.
+# - Make a list of five or more usernames called current_users.
+# - Make another list of five usernames called new_users. Make sure one or two of the usernames are also in
+# the current_users list.
+# Loop through the new_users list to see is each new username has already been used. If it has, print a message
+# that the person will need to enter a new username. If a username has not been used, print a message
+# saying that the username is available.
+# Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+
+current_users = ['admin', 'dylan', 'Brice', 'JOHN', 'JaKe!']
+new_users = ['DYlan', 'BricE', 'Ben', 'eli', 'marK']
+
+current_users2 = []
+new_users2 = []
+
+for name in current_users:
+    current_users2.append(name.lower())
+for names in new_users:
+    new_users2.append(names.lower())
+
+for item in current_users2:
+    if item in new_users2:
+        print('That username is already taken. Please choose a different one.')
+    else:
+        print('That username is valid.')
+        
+# 5.11 Ordinal Numbers: Odinal numbers indicate their position in a list, such as 1st or 2nd.
+# Most ordinal numbers end in th, except 1, 2, and 3.
+# - Store the numbers 1 - 9 in a list
+# - Loop through the list
+# - Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number.
+# Your output should read '1st 2nd 3rd 4th 5th 6th 7th 8th 9th', and each result should be on a seperate line.
+
+ordinal_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for num in ordinal_numbers:
+    if num == 1:
+        print(str(num) + 'st')
+    elif num == 2:
+        print(str(num) + 'nd')
+    elif num == 3:
+        print(str(num) + 'rd')
+    else:
+        print(str(num) + 'th')
